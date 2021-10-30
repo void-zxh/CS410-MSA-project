e38c628783575f80a0826d7200413a1a87f4279c
void-zxh <58631543+void-zxh@users.noreply.github.com>
2021-10-30 12:24:38 +0800

    Update Tool.h

--- Tool.h
+++ Tool.h
@@ -11,12 +11,18 @@ using namespace std;
 const int inf=0x3f3f3f3f;
 const int GAP=2;
 const int MISMATCH=3;
-const int EPOCH=400;
-const int LIM_GEN=100;
+const int ID_EPOCH=50;
+const int ID_EPOCH_3=20;
+const int EPOCH=100;
+const int EPOCH_3=100;
+const int LIM_GEN=10;
+const int LIM_GEN_3=10;
 const int LIM_SPACE_MAX=20;//20
 const int LIM_SPACE_MIN=8;//8
 const int ID_CHROM_NUM=65;
-const int CHROM_BUM=130;
+const int ID_CHROM_NUM_3=65;
+const int CHROM_NUM=390;
+const int CHROM_NUM_3=26;
 //const float P_CROSS=0.3;
 //const float P_MUT=0.3;
 //const float P_DBMUT=0.3;
@@ -60,15 +66,15 @@ struct ASTAR_node3
 	}
 };
 
-struct GA_node2
+struct ID_GA_node2
 {
     int lenx,leny,final_len;
     int query[MAX_LEN+5];
     int comp[MAX_LEN+5];
     int vis[MAX_LEN*2+10];
     int fitness;
-    GA_node2(int lx=0,int ly=0){lenx=lx;leny=ly;fitness=inf;}
-    bool operator < (GA_node2 a)const
+    ID_GA_node2(int lx=0,int ly=0){lenx=lx;leny=ly;fitness=inf;}
+    bool operator < (ID_GA_node2 a)const
 	{
 		if(fitness==a.fitness)
         {
@@ -84,7 +90,7 @@ struct GA_node2
                     for(int i=1;i<=leny;i++)
                         if(comp[i]<a.comp[i])
                             return 1;
-                        else if(comp[i]>a.query[i])
+                        else if(comp[i]>a.comp[i])
                             return 0;
                     return 0;
                 }
@@ -96,6 +102,62 @@ struct GA_node2
         else
             return fitness<a.fitness;
 	}
+};
+
+struct ID_GA_node3
+{
+    int lenx,leny,lenz,final_len;
+    int query[MAX_LEN+5];
+    int compy[MAX_LEN+5];
+    int compz[MAX_LEN+5];
+    int vis[MAX_LEN*2+10];
+    int fitness;
+    ID_GA_node3(int lx=0,int ly=0,int lz=0){lenx=lx;leny=ly;lenz=lz;fitness=inf;}
+    bool operator < (ID_GA_node3 a)const
+	{
+        if(fitness==a.fitness)
+        {
+            if(lenx==a.lenx)
+            {
+                if(leny==a.leny)
+                {
+                    if(lenz==a.lenz)
+                    {
+                        for(int i=1;i<=lenx;i++)
+                            if(query[i]<a.query[i])
+                                return 1;
+                            else if(query[i]>a.query[i])
+                                return 0;
+                        for(int i=1;i<=leny;i++)
+                            if(compy[i]<a.compy[i])
+                                return 1;
+                            else if(compy[i]>a.compy[i])
+                                return 0;
+                        for(int i=1;i<=lenz;i++)
+                            if(compz[i]<a.compz[i])
+                                return 1;
+                            else if(compz[i]>a.compz[i])
+                                return 0;
+                        return 0;
+                    }
+                    return lenz<a.lenz;
+                }
+                return leny<a.leny;
+            }
+            else
+                return lenx<a.lenx;
+        }
+        else
+            return fitness<a.fitness;
+    }
+};
+
+struct ID_GA_ans3
+{
+    int idx,idy;
+    int ans;
+    ID_GA_node3 ans_output;
+    ID_GA_ans3(int ix=1,int iy=2,int re=inf){idx=ix;idy=iy;ans=re;}
 };
 
 void visual_generate_2(Node2** pre_2,char* query_input,int len_input,char* query_comp,int len)
@@ -134,6 +196,29 @@ void visual_generate_2(Node2** pre_2,char* query_input,int len_input,char* query
         cur=cur_state.y;
     }
     printf("\n\n");
+}
+
+void fitness_update_2(ID_GA_node2& xi,char* query_input,char* query_x)
+{
+    int ix,iy;
+        xi.fitness=0; ix=1; iy=1;
+        for(int k=1;k<=xi.final_len;k++)
+            if(xi.vis[k]==1)
+            {
+                xi.fitness+=GAP;
+                iy++;
+            }
+            else if(xi.vis[k]==2)
+            {
+                xi.fitness+=GAP;
+                ix++;
+            }
+            else 
+            {
+                if(query_input[ix]!=query_x[iy])
+                    xi.fitness+=MISMATCH;
+                ix++; iy++;
+            }
 }
 
 void visual_generate_3(Node3*** pre_3,char* query_input,int len_input,char* query_x,int lenx,char* query_y,int leny)
